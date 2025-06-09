0506cbfe7a2e2d89ec369c0108bb0676af801ae5
robopuffin <robopuffin@gmail.com>
2025-06-09 11:59:54 -0400

    Update safeguard_logic.py
    Getting Started

--- src/safeguard_logic.py
+++ src/safeguard_logic.py
@@ -1,1 +1,7 @@
-
+def decide_action(confidence, stopping_distance, distance_to_object):
+    if confidence < 0.4 and stopping_distance >= distance_to_object:
+        return 'ABORT'
+    elif confidence < 0.6:
+        return 'SLOW'
+    else:
+        return 'GO'
